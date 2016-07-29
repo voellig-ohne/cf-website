4720d63cc4330fb6fd3fd27ea8c7a6c929faec53
Timur Çelikel <me@timurcelikel.com>
2016-07-29 11:40:16 +0200

    add some fake sections

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,12 +18,45 @@ export default class Index extends React.Component {
                             className="intro-wald" />
                 </section>
                 <section className="section_content">
-                    <div className="">
+                    <div className="section_content-main">
                         <h1>Alles für e-Mail Marketing.</h1>
-                        <p>Ein Paragraph.</p>
+                        <p>
+                            Wir sehen E-Mail Marketing langfristig. Eine
+                            Zusammenwirkung zwischen Marketing/CRM und
+                            IT-Abteilung. Zielgenaue Kommunikation mit dem
+                            Kunden. Weniger ist mehr. Dafür passender und
+                            wertvoller.
+                        </p>
+                    </div>
+                    <div className="section_content-secondary">
+                        <div className="section_content-placehoder_graphic">
+                            <div>
+                                <p>eine grafik:</p>
+                                <p>Umsetzung, Beratung, Workshops.</p>
+                            </div>
+                        </div>
                     </div>
                 </section>
-                <section className="section_content"></section>
+                <section className="section_content">
+                    <div className="section_content-secondary">
+                        <div className="section_content-placehoder_graphic">
+                            <div>
+                                <p>eine grafik:</p>
+                                <p>Umsetzung, Beratung, Workshops.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="section_content-main">
+                        <h1>Alles für e-Mail Marketing.</h1>
+                        <p>
+                            Wir sehen E-Mail Marketing langfristig. Eine
+                            Zusammenwirkung zwischen Marketing/CRM und
+                            IT-Abteilung. Zielgenaue Kommunikation mit dem
+                            Kunden. Weniger ist mehr. Dafür passender und
+                            wertvoller.
+                        </p>
+                    </div>
+                </section>
                 <section className="section_content"></section>
               </div>
             </DocumentTitle>
