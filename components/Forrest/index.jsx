c2f6266c92afcf1077eaf6e432c7f4d9dc861a21
Timur Çelikel <me@timurcelikel.com>
2016-08-04 16:03:54 +0200

    forrest: add fox

--- components/Forrest/index.jsx
+++ components/Forrest/index.jsx
@@ -6,7 +6,7 @@ import classNames from 'classNames'
 
 const ELEMENT_SIZE = {
     width: 30,
-    height: 30
+    height: 28
 }
 
 export default class Forrest extends React.Component {
@@ -17,6 +17,10 @@ export default class Forrest extends React.Component {
             size: {
                 height: 0,
                 width: 0
+            },
+            foxPosition: {
+                row: 0,
+                column: 0
             }
         }
     }
@@ -27,52 +31,80 @@ export default class Forrest extends React.Component {
         }
     }
     onResize() {
-        this.setState({
-            size: {
-                height: this.container.offsetHeight,
-                width: this.container.offsetWidth
-            }
-        })
-    }
-    generateGrid(size, ELEMENT_SIZE) {
-        const gridDimentions = {
-            height: size.height / ELEMENT_SIZE.height,
-            width: size.width / ELEMENT_SIZE.width
+        const windowSize = {
+            height: this.container.offsetHeight,
+            width: this.container.offsetWidth
         }
 
-        return Array.apply(null, {length: gridDimentions.height}).map(() => {
-            return Array.apply(null, {length: gridDimentions.width}).map(() => {
+        const gridDimentions = this.getGridDimentions(windowSize, ELEMENT_SIZE)
+        const foxPosition = this.generateFoxPosition(gridDimentions)
+        const grid = this.generateGrid(gridDimentions)
+
+        this.setState({
+            grid,
+            foxPosition
+        })
+    }
+    generateGrid(gridDimentions) {
+        return Array.apply(null, {length: gridDimentions.height}).map((row, rowIndex) => {
+            return Array.apply(null, {length: gridDimentions.width}).map((column, columnIndex) => {
                 return {
                     isActive: Math.random() < .8
                 }
             })
         })
     }
+    generateFoxPosition(gridDimentions) {
+        return {
+            row: Math.round(gridDimentions.width * Math.random()),
+            column: Math.round(gridDimentions.height * Math.random())
+        }
+    }
+    getGridDimentions(windowSize, elementSize) {
+        return {
+            height: windowSize.height / elementSize.height,
+            width: windowSize.width / elementSize.width
+        }
+    }
+    onMouseEnter() {
+    }
+    isFox(currentPosition, foxPosition) {
+        return currentPosition.row === foxPosition.row &&
+                (currentPosition.column === foxPosition.column ||
+                currentPosition.column + 1 === foxPosition.column)
+    }
     render () {
         const { className } = this.props
-
-        const grid = this.generateGrid(this.state.size, ELEMENT_SIZE)
 
         return (
             <div className={ classNames('forrest', className) }
                 ref={(c) => this.container = c}>
                 {
-                    map(grid, (row, idxRow) => {
+                    map(this.state.grid, (row, idxRow) => {
                         return (
                             <div key={idxRow}
                                     className="forrest-row">
                                 {
                                     map(row, (element, idxElement) => {
+
+                                        const isFox = this.isFox(
+                                            { row: idxRow, column: idxElement},
+                                            this.state.foxPosition
+                                        )
+
                                         const classes = classNames(
                                             'forrest-element',
                                             {
-                                                'forrest-element--active': element.isActive
+                                                'forrest-element--active': element.isActive,
+                                                'forrest-element--fox': isFox
                                             }
                                         )
+
                                         return (
                                             <Illustration illustration="element"
                                                 className={classes}
-                                                key={idxElement} />
+                                                key={idxElement}
+                                                onMouseEnter={this.onMouseEnter.bind(this)} />
                                         )
                                     })
                                 }
