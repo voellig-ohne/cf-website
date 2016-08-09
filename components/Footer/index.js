864227d0f7c4ff92a6c485ca0ed89ddbe1feb713
Timur Çelikel <me@timurcelikel.com>
2016-08-09 16:37:56 +0200

    footer: add imprint

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -7,7 +7,27 @@ module.exports = React.createClass({
         return (
             <footer className="footer"
                 id="footer">
-                <section className="footer-section"></section>
+                <section className="footer-section footer-imprint">
+                    <p>
+                        fuchs+wald<br />
+                        <em>digitaldialoge</em>
+                    </p>
+                    <p>
+                        Sonnenallee 206<br />
+                        12059 Berlin<br />
+                        +49 (0) 162 24 16 317
+                    </p>
+                    <p>
+                        fuchs@undwald.de
+                    </p>
+                    <p>
+                        St.-Nr.: 16/123/12345<br />
+                        Iban: DE34 12094995873 7366473
+                    </p>
+                    <p>
+                        fuchs.undwald.de
+                    </p>
+                </section>
                 <section className="footer-section">
                     <h2>schreib mir.</h2>
                     <input type="text" placeholder="name"/>
