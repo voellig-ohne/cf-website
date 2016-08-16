fa8a063138c011543c63638203da30df0b45a37f
Timur Çelikel <me@timurcelikel.com>
2016-08-16 16:40:18 +0200

    landing page: use actual forrest illustration

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,7 @@ import { prefixLink } from 'gatsby-helpers'
 import { config } from 'config'
 import Illustration from '../components/Illustration'
 import Hero from '../components/Hero'
+import Forrest from '../components/Forrest'
 
 export default class Index extends React.Component {
     render() {
@@ -57,90 +58,45 @@ export default class Index extends React.Component {
                         </div>
                     </div>
                 </section>
-                <section className="section_content">
-                    <div className="section_content-main">
-                        <h1>Wir finden den richtigen Weg durch den Wald.</h1>
-                        <p>
-                            (LEISTUNGEN) Analyse Umsetzung Template-
-                            Programmierung Projektmanagement Design
-                            Versandunterstützung Seminare & Workshops Es
-                            id quibusdam quam aut quia volum fugia culloriat.
-                            Atur alicatur a del is eum natis maio. Itam explaut
-                            quibus et asperciendam rendus delectus que nonsequi
-                            doluptius, idellessit acesto quo molum dello et, sus.
-                            <br />
-                            <Link to="/leistungen/">mehr...</Link>
-                        </p>
-                    </div>
-                    <div className="section_content-secondary">
-                        <div className="section_content-graphic">
-                            <img src="intro-durch-den-wald.png"
-                                className="section_content-graphic-image"/>
-                        </div>
-                    </div>
-                </section>
-                <section className="section_content">
-                    <div className="section_content-secondary">
-                        <div className="section_content-graphic">
-                            <img src="intro-kennt-sich-aus.png"
-                                className="section_content-graphic-image section_content-graphic-image--fuchs"/>
-                        </div>
-                    </div>
-                    <div className="section_content-main">
-                        <h1>Der Fuchs kennt sich aus.</h1>
-                        <p>
-                            (Arbeitsweise) Analyse Umsetzung Template-
-                            Programmierung Projektmanagement Design
-                            Versandunterstützung Seminare & Workshops Es
-                            id quibusdam quam aut quia volum fugia culloriat.
-                            Atur alicatur a del is eum natis maio. Itam explaut
-                            quibus et asperciendam rendus delectus que nonsequi
-                            doluptius, idellessit acesto quo molum dello et, sus.
-                        </p>
-                    </div>
-                </section>
-                <section className="section_content">
-                    <div className="section_content-main">
-                        <h1>Für jeden Wald der richtige Fuchs.</h1>
-                        <p>
-                            (Arbeitsweise) Analyse Umsetzung Template-
-                            Programmierung Projektmanagement Design
-                            Versandunterstützung Seminare & Workshops Es
-                            id quibusdam quam aut quia volum fugia culloriat.
-                            Atur alicatur a del is eum natis maio. Itam explaut
-                            quibus et asperciendam rendus delectus que nonsequi
-                            doluptius, idellessit acesto quo molum dello et, sus.
-                        </p>
-                    </div>
-                    <div className="section_content-secondary">
-                        <div className="section_content-graphic">
-                            <img src="intro-jeder-wald-richtiger-fuchs.png"
-                                className="section_content-graphic-image"/>
-                        </div>
-                    </div>
-                </section>
-                <section className="section_content">
-                    <div className="section_content-secondary">
-                        <div className="section_content-graphic">
-                            <img src="intro-jeder-kann-fuchs.png"
-                                className="section_content-graphic-image section_content-graphic-image--fuchse"/>
-                        </div>
-                    </div>
-                    <div className="section_content-main">
-                        <h1>Jeder kann ein Fuchs sein.</h1>
-                        <p>
-                            (Arbeitsweise) Analyse Umsetzung Template-
-                            Programmierung Projektmanagement Design
-                            Versandunterstützung Seminare & Workshops Es
-                            id quibusdam quam aut quia volum fugia culloriat.
-                            Atur alicatur a del is eum natis maio. Itam explaut
-                            quibus et asperciendam rendus delectus que nonsequi
-                            doluptius, idellessit acesto quo molum dello et, sus.
-                        </p>
-                    </div>
-                </section>
+                <IntroSection
+                        illustration={(<Forrest
+                            mode="single"
+                            className="section_content-graphic-image"/>)}>
+                    <h1>Wir finden den richtigen Weg durch den Wald.</h1>
+                    <p>
+                        (LEISTUNGEN) Analyse Umsetzung Template-
+                        Programmierung Projektmanagement Design
+                        Versandunterstützung Seminare & Workshops Es
+                        id quibusdam quam aut quia volum fugia culloriat.
+                        Atur alicatur a del is eum natis maio. Itam explaut
+                        quibus et asperciendam rendus delectus que nonsequi
+                        doluptius, idellessit acesto quo molum dello et, sus.
+                        <br />
+                        <Link to="/leistungen/">mehr...</Link>
+                    </p>
+                </IntroSection>
               </main>
             </DocumentTitle>
         )
     }
+}
+
+class IntroSection extends React.Component {
+    render() {
+        const { illustration } = this.props
+        console.log(this)
+        return (
+            <section className="section_content">
+                <div className="section_content-main">
+                    { this.props.children }
+                </div>
+                <div className="section_content-secondary">
+                    <div className="section_content-graphic">
+                        { illustration }
+                    </div>
+                </div>
+            </section>
+        )
+    }
+
 }
