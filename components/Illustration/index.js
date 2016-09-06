eea1048296880dd015d86e2427baac96cff31b9b
Timur Çelikel <me@timurcelikel.com>
2016-09-06 12:33:59 +0200

    upgrade gatsby to 0.12.4

--- components/Illustration/index.js
+++ components/Illustration/index.js
@@ -5,7 +5,7 @@ export default class Illustration extends React.Component {
     render () {
         const { illustration, className, ...other } = this.props
 
-        const svg = require('./illustrations/' + illustration + '.svg')
+        const svg = require('!svg-inline-loader!./illustrations/' + illustration + '.svg')
 
         return (
             <div dangerouslySetInnerHTML={{ __html: svg }}
