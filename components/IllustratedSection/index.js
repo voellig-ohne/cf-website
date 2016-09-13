6c59775d28567c4bcfa7766cb68b9108f4fbe58f
Timur Çelikel <me@timurcelikel.com>
2016-09-13 18:41:15 +0200

    IllustratedSection: add component

---
+++ components/IllustratedSection/index.js
@@ -0,0 +1,26 @@
+import Illustration from 'components/Illustration'
+import React from 'react'
+import style from './style.module.less'
+import { map } from 'lodash'
+import classNames from 'classNames'
+
+export default class IllustratedSection extends React.Component{
+    render() {
+        const { illustration, title } = this.props
+        const className = classNames(this.props.className, style.section)
+
+        console.log(style)
+
+        return (
+            <section className={ className }>
+                <div className={ style['illustration-wrap'] }>
+                    <Illustration illustration={ illustration } className={style.illustration} />
+                    <h2 className={ style.title }>{ title }</h2>
+                </div>
+                <div className={ style.text }>
+                    { this.props.children }
+                </div>
+            </section>
+        )
+    }
+}
