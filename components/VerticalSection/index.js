5647b3d219d824b2862a1b3e3a6ee96566168ea7
Timur Çelikel <me@timurcelikel.com>
2016-09-28 15:47:24 +0200

    add VerticalSection element

---
+++ components/VerticalSection/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import style from './style.module.less'
+import classNames from 'classNames'
+import { map } from 'lodash'
+import Illustration from 'components/Illustration'
+
+export default class VerticalSection extends React.Component {
+    render () {
+        const { sections } = this.props
+
+        return (
+            <section className="section_content section_content--single">
+                <div className={classNames('section_content-single', 'section_content-single--wide')}>
+                    <div className={style.sections}>
+                        {
+                            map(sections, (section, idx) => {
+                                return (
+                                    <div key={idx}
+                                            className={style.section}>
+                                        <div className={style.illustration_container}>
+                                            <Illustration illustration={section.icon} className={style.illustration} />
+                                        </div>
+                                        <h2>{ section.title }</h2>
+                                        <div dangerouslySetInnerHTML={{ __html: section.body }} />
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            </section>
+        )
+    }
+}
