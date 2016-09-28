5782881523063a311bb7b2c1e3a9e86ca8d3c211
Timur Çelikel <me@timurcelikel.com>
2016-09-28 15:47:33 +0200

    add Arbeitsweisen

--- pages/arbeitsweisen/index.jsx
+++ pages/arbeitsweisen/index.jsx
@@ -4,16 +4,26 @@ import DocumentTitle from 'react-document-title'
 import { prefixLink } from 'gatsby-helpers'
 import { config } from 'config'
 import Forrest from 'components/Forrest'
+import VerticalSection from 'components/VerticalSection'
 import Hero from 'components/Hero'
+import { filter, map } from 'lodash'
 
 export default class Arbeitsweisen extends React.Component {
     render() {
+        const weisen = map(filter(this.props.route.pages, (page) => {
+            return page.path.includes('/arbeitsweisen/weisen')
+        }), (page) => {
+            return page.data
+        })
+
         return (
             <DocumentTitle title={ config.siteTitle + ' | arbeitsweisen'  }>
                 <main>
                     <section className="section_content">
                         <h1 className="page_heading">Arbeitsweisen</h1>
                     </section>
+
+                    <VerticalSection sections={weisen} />
 
                     <Hero image="/img/wald_briefe.jpg">
                         <Link to="/usecases/" className="cta">
