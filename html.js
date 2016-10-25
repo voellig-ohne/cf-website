d84e2f9b82a818df84e1afb1445d83257af663f2
Timur Çelikel <me@timurcelikel.com>
2016-10-25 13:59:01 +0200

    add favicon

--- html.js
+++ html.js
@@ -24,6 +24,7 @@ module.exports = React.createClass({
               <meta charSet="utf-8" />
               <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
               <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=5.0" />
+              <link rel="shortcut icon" href="/favicon.png" />
               <title>
                 { title }
               </title>
