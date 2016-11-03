83947c671eb1670531ae46d89b4bfca09ee70fe4
Timur Çelikel <me@timurcelikel.com>
2016-11-03 15:09:50 +0100

    page for forrest projection

---
+++ pages/projection/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router'
+import DocumentTitle from 'react-document-title'
+import { config } from 'config'
+import { filter, map, includes } from 'lodash'
+import Forrest from 'components/Forrest'
+import style from './style.module.less'
+import classNames from 'classNames'
+
+export default class UseCases extends React.Component {
+    render() {
+        const cases = filter(this.props.route.pages, (page) => {
+            return includes(page.path, '/usecases/cases/')
+        })
+
+        return (
+            <DocumentTitle title={ config.siteTitle + ' | project!' }>
+                <main>
+                    <section className="sub_intro">
+                        <div className="sub_intro-forrest">                            
+                            <Forrest className={style.canvas}
+                                mode="single"
+                                projection={true}/>
+                        </div>
+                    </section>
+                </main>
+            </DocumentTitle>
+        )
+    }
+}
