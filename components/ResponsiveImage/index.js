c9289290fcd5022ef90b4b8b488db34b01b24270
Timur Çelikel <me@timurcelikel.com>
2016-12-20 12:06:34 +0100

    fix broken images, don't resize images in develop mode

--- components/ResponsiveImage/index.js
+++ components/ResponsiveImage/index.js
@@ -2,22 +2,33 @@ import React from 'react'
 
 export default class ResponsiveImage extends React.Component {
     render () {
-        const { source, location, alt, className } = this.props
-        const linkPrefix = (process.env.NODE_ENV === 'production') ? '/' : ''
+        const { source, alt, className } = this.props
 
-        const size2 = linkPrefix + require('!file!scale?size=1000!../.././pages/img/' + source + '.jpg')
-        const size3 = linkPrefix + require('!file!scale?size=1500!../.././pages/img/' + source + '.jpg')
-        const size4 = linkPrefix + require('!file!scale?size=2000!../.././pages/img/' + source + '.jpg')
-        const size5 = linkPrefix + require('!file!scale?size=2500!../.././pages/img/' + source + '.jpg')
-        const size6 = linkPrefix + require('!file!scale?size=3000!../.././pages/img/' + source + '.jpg')
+        const location = '/img/'
 
-        const srcSet = size2 + ' 1000w, ' + size3 + ' 1500w, ' + size4 + ' 2000w, ' + size5 + ' 2500w, ' + size6 + ' 3000w'
+        if (process.env.NODE_ENV !== 'production') {
+            const path = location + source + '.jpg'
+            return (
+                <img key={source} src={path} className={className} alt={alt} />
+            )
+        } else {
+            const path = location.substr(1)
 
-        return (
-            <img src={size2}
-                    srcSet={srcSet}
-                    alt={alt}
-                    className={className}/>
-        )
+            const size2 = require('!file!scale?size=1000!../.././pages/' + path + source + '.jpg')
+            const size3 = require('!file!scale?size=1500!../.././pages/' + path + source + '.jpg')
+            const size4 = require('!file!scale?size=2000!../.././pages/' + path + source + '.jpg')
+            const size5 = require('!file!scale?size=2500!../.././pages/' + path + source + '.jpg')
+            const size6 = require('!file!scale?size=3000!../.././pages/' + path + source + '.jpg')
+
+            const srcSet = size2 + ' 1000w, ' + size3 + ' 1500w, ' + size4 + ' 2000w, ' + size5 + ' 2500w, ' + size6 + ' 3000w'
+
+            return (
+                <img key={source}
+                        src={size2}
+                        srcSet={srcSet}
+                        alt={alt}
+                        className={className}/>
+            )
+        }
     }
 }
