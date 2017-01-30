3ec8ae914cbee9d0a1e6cf4b597443c99b6e895c
Timur Çelikel <me@timurcelikel.com>
2017-01-30 16:41:15 +0100

    PartnerList: layout

--- components/PartnerList/index.js
+++ components/PartnerList/index.js
@@ -8,8 +8,6 @@ export default class PartnerList extends React.Component {
     render () {
         const { partners } = this.props
 
-        console.log(partners)
-
         return (
             <ul className={ style.partners }>
                 {
@@ -17,18 +15,30 @@ export default class PartnerList extends React.Component {
                         return (
                             <li key={idx}
                                     className={ style.partner }>
-                                <div className={ style.image_container }>
-                                    { partner.data.img ?
-                                        <ResponsiveImage source={partner.data.img} 
-                                            className={ style.image }
-                                            sizes="(max-width: 600px) 100vw, 400px"/>
-                                        : null
-                                    }
+                                <div className={ style.image_container_container }>
+                                    <div className={ style.image_container }>
+                                        { partner.data.img ?
+                                            <ResponsiveImage source={partner.data.img} 
+                                                className={ style.image }
+                                                sizes="(max-width: 600px) 100vw, 400px"/>
+                                            : null
+                                        }
+                                    </div>
                                 </div>
                                 
-                                { partner.data.name }
+                                <h2 className={ style.heading }>
+                                    { partner.data.name }
+                                </h2>
 
+                                <div  className={ style.text }
+                                    dangerouslySetInnerHTML={{ __html: partner.data.body }} />
 
+                                { partner.data.link ?
+                                    <a href={ partner.data.link.href }
+                                            target="_blank">
+                                        { partner.data.link.text }
+                                    </a> : null
+                                }
                             </li>
                         ) 
                     })
