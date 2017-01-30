938b85e9a89dc6a1d45a18158e8945cb3c020ace
Timur Çelikel <me@timurcelikel.com>
2017-01-30 16:15:02 +0100

    PartnerList: basic layout

--- components/PartnerList/index.js
+++ components/PartnerList/index.js
@@ -2,10 +2,13 @@ import React from 'react'
 import classNames from 'classNames'
 import style from './style.module.less'
 import { map } from 'lodash'
+import ResponsiveImage from 'components/ResponsiveImage'
 
 export default class PartnerList extends React.Component {
     render () {
         const { partners } = this.props
+
+        console.log(partners)
 
         return (
             <ul className={ style.partners }>
@@ -14,7 +17,17 @@ export default class PartnerList extends React.Component {
                         return (
                             <li key={idx}
                                     className={ style.partner }>
+                                <div className={ style.image_container }>
+                                    { partner.data.img ?
+                                        <ResponsiveImage source={partner.data.img} 
+                                            className={ style.image }
+                                            sizes="(max-width: 600px) 100vw, 400px"/>
+                                        : null
+                                    }
+                                </div>
+                                
                                 { partner.data.name }
+
 
                             </li>
                         ) 
