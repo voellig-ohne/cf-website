4ad4bc0d810486aa278f2b5c5a1b5ffc71d524f1
Timur Çelikel <me@timurcelikel.com>
2017-03-29 22:04:19 +0200

    Events: styling

--- components/Events/index.js
+++ components/Events/index.js
@@ -6,20 +6,41 @@ import SectionContentSingle from 'components/SectionContentSingle'
 import moment from 'moment';
 import 'moment/locale/de';
 
+const TYPES = {
+    open: 'Offener Workshop',
+    individual: 'Individueller Workshop',
+    inhouse: 'Inhouse Workshop'
+}
+
 function Events({ events }) {
     return (
         <SectionContentSingle wide={true}>
             <ul className={style.events}>
                 { map(events, (event) => {
-                    console.log(event)
                     return (
                         <li key={event.title}
                                 className={style.event}>
                             <div className={style.meta}>
                                 { event.date && <Date date={event.date} /> } 
+                                <div>
+                                    { event.price && 
+                                        <Meta> 
+                                            <span>Preis</span>
+                                            <span>{event.price}</span>
+                                        </Meta>
+                                    } 
+                                    { event.type && 
+                                        <Meta> 
+                                            <span>Typ</span>
+                                            <span>{ TYPES[event.type] }</span>
+                                        </Meta>
+                                    } 
+                                </div>
                             </div>
-                            <div className={style.meta}>
-                                <h2>{ event.title }</h2>
+                            <div className={style.content}>
+                                <h2 className={style.heading}>
+                                    { event.title }
+                                </h2>
                                 <div className={style.body}
                                         dangerouslySetInnerHTML={{ __html: event.body }} />
                             </div>
@@ -31,12 +52,37 @@ function Events({ events }) {
     )
 }
 
+function Meta({ children }) {
+    return (
+        <div className={style.meta_info}>
+            <span className={style.meta_info_key}>
+                { children[0] }
+            </span>:&nbsp;
+            <span className={style.meta_info_value}>
+                { children[1] }
+            </span>
+        </div>
+    )
+}
+
 function Date({ date }) {
     const d = moment(date);
 
     return (
         <div className={style.date}>
-            { d.format('MMMM') }            
+            <div className={style.day}>
+                { d.format('DD.MM.') }
+            </div>
+            <div className={style.year}>
+                { map(d.format('YYYY'), (number, idx) => {
+                    return (
+                        <span key={idx}>
+                            { number }
+                        </span>
+                    );
+                    })
+                 }
+            </div>
         </div>
     )
 }
