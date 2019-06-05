a9d2aeca9daf7f019b1e92d99e1c3e344ba8e1fe
Timur Çelikel <me@timurcelikel.com>
2019-06-05 11:03:05 +0200

    add link to blog

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,47 +1,63 @@
-import React from 'react'
-import './style.less'
-import { Link } from 'react-router'
-import classNames from 'classNames'
+import React from 'react';
+import './style.less';
+import { Link } from 'react-router';
+import classNames from 'classNames';
 
 export default class Navigation extends React.Component {
-    render () {
+    render() {
         const links = [
             {
-                link: '/ueber_uns/',
-                title: 'über uns'
-            },{
-                link: '/leistungen/',
-                title: 'leistungen'
-            },{
-                link: '/arbeitsweisen/',
-                title: 'arbeitsweisen'
-            },{
-                link: '/usecases/',
-                title: 'use cases'
-            },{
-                link: '/workshops/',
-                title: 'workshops'
-            }
-        ]
+                to: '/ueber_uns/',
+                title: 'über uns',
+            },
+            {
+                to: '/leistungen/',
+                title: 'leistungen',
+            },
+            {
+                to: '/arbeitsweisen/',
+                title: 'arbeitsweisen',
+            },
+            {
+                to: '/usecases/',
+                title: 'use cases',
+            },
+            {
+                to: '/workshops/',
+                title: 'workshops',
+            },
+            {
+                href: 'https://blog.fuchsundwald.de/',
+                title: 'blog',
+            },
+        ];
 
-        const { location } = this.props
+        const { location } = this.props;
 
         return (
             <nav className="navigation">
                 <ul className="navigation-list">
-                    {links.map((link) => {
-                        const linkClasses = classNames('navigation_link', {'navigation_link--active': link.link === location})
+                    {links.map(link => {
+                        const linkClasses = classNames('navigation_link', {
+                            'navigation_link--active': link.link === location,
+                        });
                         return (
-                            <li className="navigation-item"
-                                    key={link.link}>
-                                <Link to={link.link} className={linkClasses}>
-                                    {link.title}
-                                </Link>
+                            <li className="navigation-item" key={link.link}>
+                                {link.to && (
+                                    <Link to={link.to} className={linkClasses}>
+                                        {link.title}
+                                    </Link>
+                                )}
+                                {link.href && (
+                                    <a href={link.href} className={linkClasses}>
+                                        {link.title}
+                                    </a>
+                                )}
                             </li>
-                        )
+                        );
                     })}
                 </ul>
             </nav>
-        )
+        );
     }
 }
