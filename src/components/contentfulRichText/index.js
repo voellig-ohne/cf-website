625de9b37f5090ed238d546128dffba1e3060731
Timur Çelikel <me@timurcelikel.com>
2020-07-07 16:57:04 +0200

    add first intro section

--- src/components/contentfulRichText/index.js
+++ src/components/contentfulRichText/index.js
@@ -1,5 +1,15 @@
+import React from 'react';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 export default (json) => {
-    return documentToReactComponents(json, {});
+    const options = {
+        // needed so that line breaks are properly added.
+        renderText: (text) => {
+            return text.split('\n').reduce((children, textSegment, index) => {
+                return [...children, index > 0 && <br key={index} />, textSegment];
+            }, []);
+        },
+    };
+
+    return documentToReactComponents(json, options);
 };
