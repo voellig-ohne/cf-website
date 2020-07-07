548eb37dddc0fd5d0f06e24e2e4f0199fe1d0de5
Timur Çelikel <me@timurcelikel.com>
2020-07-07 14:52:09 +0200

    add meta stuff: title, description

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -3,8 +3,10 @@ import { graphql } from 'gatsby';
 import Navigation from '../Navigation';
 import Footer from '../Footer';
 import LogoContainer from '../LogoContainer';
+import Favicon from './favicon.png';
 
 import '../style/main.less';
+import { Helmet } from 'react-helmet-async';
 
 export default ({ data, location }) => {
     const { contentfulGlobal, contentfulPage } = data;
@@ -12,6 +14,11 @@ export default ({ data, location }) => {
 
     return (
         <>
+            <Helmet>
+                <title>{contentfulGlobal.title}</title>
+                <meta name="description" content={contentfulGlobal.description.description} />
+                <link rel="icon" type="image/png" href={Favicon} />
+            </Helmet>
             <Navigation location={location.pathname} />
             <Footer />
             <LogoContainer className="logo-container" />
@@ -23,6 +30,9 @@ export const pageQuery = graphql`
     query StaticPageBySlug($slug: String!) {
         contentfulGlobal(contentful_id: { eq: "6KsBufkTaS3SQwjqkwflQU" }) {
             title
+            description {
+                description
+            }
         }
         contentfulPage(slug: { eq: $slug }) {
             title
