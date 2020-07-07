15a7c611ca9bf4130fb42fc9e533b5a61a1259dc
Timur Çelikel <me@timurcelikel.com>
2020-07-07 16:07:51 +0200

    refactor: separate Page from PageWrapper

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,46 +1,29 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import Navigation from '../Navigation';
-import Footer from '../Footer';
-import LogoContainer from '../LogoContainer';
-import Favicon from './favicon.png';
-
 import '../style/main.less';
 import { Helmet } from 'react-helmet-async';
 import SectionContentSingle from '../SectionContentSingle';
 import contentfulRichText from '../contentfulRichText';
+import PageWrapper from '../PageWrapper';
 
-export default ({ data, location }) => {
-    const { contentfulGlobal, contentfulPage } = data;
-    console.log(data, location);
-
+export default ({ data: { contentfulPage }, location: { pathname } }) => {
     return (
-        <>
+        <PageWrapper pathname={pathname}>
             {contentfulPage.sections.map((section, index) => (
                 <SectionContentSingle key={index} title={section.titleDisplay}>
                     {contentfulRichText(section.body.json)}
                 </SectionContentSingle>
             ))}
             <Helmet>
-                <title>{contentfulGlobal.title}</title>
-                <meta name="description" content={contentfulGlobal.description.description} />
-                <link rel="icon" type="image/png" href={Favicon} />
+                <title>{contentfulPage.title}</title>
+                <meta name="description" content={contentfulPage.description.description} />
             </Helmet>
-            <Navigation location={location.pathname} />
-            <Footer />
-            <LogoContainer className="logo-container" />
-        </>
+        </PageWrapper>
     );
 };
 
 export const pageQuery = graphql`
     query StaticPageBySlug($slug: String!) {
-        contentfulGlobal(contentful_id: { eq: "6KsBufkTaS3SQwjqkwflQU" }) {
-            title
-            description {
-                description
-            }
-        }
         contentfulPage(slug: { eq: $slug }) {
             title
             description {
--- src/components/contentfulRichText/index.js
+++ src/components/contentfulRichText/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 export default (json) => {
-    return <>{documentToReactComponents(json, {})}</>;
+    return documentToReactComponents(json, {});
 };
