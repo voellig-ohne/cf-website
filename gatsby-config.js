d877346ac0f0c07466712f07ea75c56934977b51
Timur Çelikel <me@timurcelikel.com>
2020-07-07 14:52:09 +0200

    add meta stuff: title, description

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ require('dotenv').config({
 
 module.exports = {
     plugins: [
+        'gatsby-plugin-react-helmet-async',
         {
             resolve: `gatsby-source-contentful`,
             options: {
