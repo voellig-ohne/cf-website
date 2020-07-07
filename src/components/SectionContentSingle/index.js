0df926d875047b097f9016830d0658ee7a7f8e2e
Timur Çelikel <me@timurcelikel.com>
2020-07-07 15:56:09 +0200

    SectionContentSingle: refactor to function component

--- src/components/SectionContentSingle/index.js
+++ src/components/SectionContentSingle/index.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import classNames from 'classNames';
 
-export default class SectionContentSingle extends React.Component {
-    render() {
-        const { children, wide, title, classNameWrapper, className } = this.props;
+export default ({ wide, title, classNameWrapper, className, children }) => {
+    const classes = classNames('section_content', 'section_content--single', className);
 
-        const classes = classNames('section_content', 'section_content--single', className);
+    const wrapperClasses = classNames('section_content-single', classNameWrapper, {
+        'section_content-single--wide': wide,
+    });
 
-        const wrapperClasses = classNames('section_content-single', classNameWrapper, {
-            'section_content-single--wide': wide,
-        });
-
-        return (
-            <section className={classes}>
-                <div className={wrapperClasses}>
-                    {title ? <h1 className="page_heading">{title}</h1> : null}
-                    {this.props.children}
-                </div>
-            </section>
-        );
-    }
-}
+    return (
+        <section className={classes}>
+            <div className={wrapperClasses}>
+                {title ? <h1 className="page_heading">{title}</h1> : null}
+                {children}
+            </div>
+        </section>
+    );
+};
