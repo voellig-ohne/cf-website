be5290a9dcf98acb940410f5c7470a27d09946da
Timur Çelikel <me@timurcelikel.com>
2020-07-07 16:07:51 +0200

    refactor: separate Page from PageWrapper

---
+++ src/components/PageWrapper/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import Navigation from '../Navigation';
+import Footer from '../Footer';
+import LogoContainer from '../LogoContainer';
+import Favicon from './favicon.png';
+
+import '../style/main.less';
+import { Helmet } from 'react-helmet-async';
+
+export default ({ children, pathname }) => {
+    const { contentfulGlobal } = useStaticQuery(graphql`
+        query PageWrapperQuery {
+            contentfulGlobal(contentful_id: { eq: "6KsBufkTaS3SQwjqkwflQU" }) {
+                title
+                description {
+                    description
+                }
+            }
+        }
+    `);
+
+    return (
+        <>
+            <Helmet titleTemplate={`${contentfulGlobal.title} - %s`}>
+                <title>{contentfulGlobal.title}</title>
+                <meta name="description" content={contentfulGlobal.description.description} />
+                <link rel="icon" type="image/png" href={Favicon} />
+            </Helmet>
+            {children}
+            <Navigation location={pathname} />
+            <Footer />
+            <LogoContainer className="logo-container" />
+        </>
+    );
+};
