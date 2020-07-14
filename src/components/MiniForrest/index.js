cb094ba56a1edfd5c49775a3c9a28be897d8d50d
Timur Çelikel <me@timurcelikel.com>
2020-07-14 17:59:05 +0200

    MiniForest: refactor

--- src/components/MiniForrest/index.js
+++ src/components/MiniForrest/index.js
@@ -1,43 +1,37 @@
 import Illustration from '../Illustration';
 import React from 'react';
 import style from './style.module.less';
-import { map, find } from 'lodash';
+import { map } from 'lodash';
 import classNames from 'classnames';
 
 const TREE = 't';
 const FOX = 'f';
 const EMPTY = 'e';
 
-export default class miniForrest extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        const { className, forrest, background, isSmall } = this.props;
-        const classes = classNames(style.forrest, className, { [style['forrest-small']]: isSmall });
+export default ({ className, forrest, isSmall }) => {
+    const classes = classNames(style.forrest, className, { [style['forrest-small']]: isSmall });
 
-        return (
-            <div className={classes}>
-                {map(forrest, (row, rowIdx) => {
-                    return (
-                        <div className={style.row} key={rowIdx}>
-                            {map(row, (cell, cellIdx) => {
-                                const classes = classNames(style.element, {
-                                    [style.tree]: cell === TREE,
-                                    [style.fox]: cell === FOX,
-                                    [style['element-small']]: isSmall,
-                                });
+    return (
+        <div className={classes}>
+            {map(forrest, (row, rowIdx) => {
+                return (
+                    <div className={style.row} key={rowIdx}>
+                        {map(row, (cell, cellIdx) => {
+                            const classes = classNames(style.element, {
+                                [style.tree]: cell === TREE,
+                                [style.fox]: cell === FOX,
+                                [style['element-small']]: isSmall,
+                            });
 
-                                if (cell === EMPTY) {
-                                    return <div className={classes} key={cellIdx} />;
-                                }
+                            if (cell === EMPTY) {
+                                return <div className={classes} key={cellIdx} />;
+                            }
 
-                                return <Illustration illustration="element" className={classes} key={cellIdx} />;
-                            })}
-                        </div>
-                    );
-                })}
-            </div>
-        );
-    }
-}
+                            return <Illustration illustration="element" className={classes} key={cellIdx} />;
+                        })}
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
--- src/components/MiniForrestGroup/index.js
+++ src/components/MiniForrestGroup/index.js
@@ -1,4 +1,3 @@
-import Illustration from '../Illustration';
 import React from 'react';
 import style from './style.module.less';
 import { map } from 'lodash';
@@ -7,20 +6,14 @@ import classNames from 'classnames';
 import forrests from './_forrests.js';
 import MiniForrest from '../MiniForrest';
 
-export default class miniForrest extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        const { className, forrest, background, isSmall } = this.props;
-        const classes = classNames(style.forrests, className);
+export default ({ className, isSmall }) => {
+    const classes = classNames(style.forrests, className);
 
-        return (
-            <div className={classes}>
-                {map(forrests, (forrest, idx) => {
-                    return <MiniForrest className={style.forrest} forrest={forrest} isSmall={isSmall} key={idx} />;
-                })}
-            </div>
-        );
-    }
-}
+    return (
+        <div className={classes}>
+            {map(forrests, (forrest, idx) => {
+                return <MiniForrest className={style.forrest} forrest={forrest} isSmall={isSmall} key={idx} />;
+            })}
+        </div>
+    );
+};
