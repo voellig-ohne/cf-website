1262fc98f70d1f33c88adb25e6333e1fb750f747
Timur Çelikel <me@timurcelikel.com>
2020-07-14 17:31:36 +0200

    Intro: support all header types

--- src/components/IntroSection/index.js
+++ src/components/IntroSection/index.js
@@ -1,21 +1,38 @@
 import React from 'react';
-import Main from './Main';
-import Ueber from './Ueber';
+import FoxNetworkHead from '../FoxNetworkHead';
+import Illustration from '../Illustration';
+import Forrest from '../Forrest';
+import FoxAndYou from '../FoxAndYou';
+import MiniForrestGroup from '../MiniForrestGroup';
 
 const intros = {
-    main: Main,
-    ueber: Ueber,
-    leistungen: Main,
-    arbeitsweisen: Main,
-    usecases: Main,
-    workshops: Main,
+    ueber: () => <FoxNetworkHead className="sub_intro-forrest" />,
+    leistungen: () => <Forrest className="sub_intro-forrest" mode="single" />,
+    arbeitsweisen: () => <FoxAndYou className="sub_intro-forrest" />,
+    usecases: MiniForrestGroup,
+    workshops: () => <Forrest className="sub_intro-forrest" mode="many" density="0" />,
 };
 
 export default ({ type, claim }) => {
     const IntroComponent = intros[type];
+
+    if (type === 'main') {
+        return (
+            <section className="intro">
+                <Illustration illustration="fuchs" className="intro-fuchs" />
+                <Illustration illustration="wald" className="intro-wald" />
+            </section>
+        );
+    }
+
     return (
-        <>
-            <IntroComponent claim={claim} />
-        </>
+        <section className="sub_intro">
+            <div className="sub_intro-forrest">
+                <IntroComponent />
+            </div>
+            <header className="sub_intro-header">
+                <h1 className="sub_intro-heading">{claim}</h1>
+            </header>
+        </section>
     );
 };
