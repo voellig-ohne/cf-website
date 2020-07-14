8092d562843b95c942c40877786cf3efecb7248f
Timur Çelikel <me@timurcelikel.com>
2020-07-14 16:56:07 +0200

    add ueber header

--- src/components/IntroSection/index.js
+++ src/components/IntroSection/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Main from './Main';
+import Ueber from './Ueber';
 
 const intros = {
     main: Main,
+    ueber: Ueber,
+    leistungen: Main,
+    arbeitsweisen: Main,
+    usecases: Main,
+    workshops: Main,
 };
 
 export default ({ type, claim }) => {
     const IntroComponent = intros[type];
     return (
         <>
-            <IntroComponent />
+            <IntroComponent claim={claim} />
         </>
     );
 };
