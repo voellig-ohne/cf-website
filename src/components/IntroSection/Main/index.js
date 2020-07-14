fd8be07f1f8e37677429d9af5dd0c9f296cff848
Timur Çelikel <me@timurcelikel.com>
2020-07-14 17:31:36 +0200

    Intro: support all header types

--- src/components/IntroSection/Main/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Illustration from '../../Illustration';
-
-export default () => (
-    <section className="intro">
-        <Illustration illustration="fuchs" className="intro-fuchs" />
-        <Illustration illustration="wald" className="intro-wald" />
-    </section>
-);
