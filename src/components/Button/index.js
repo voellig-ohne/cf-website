ab0c4409026109651a39f66fa1262076a9c3d695
Timur Çelikel <me@timurcelikel.com>
2020-07-15 15:23:20 +0200

    Buttons: add size property

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import classNames from 'classnames';
 
-export default ({ label, targetLink, targetPage }) => {
+export default ({ label, targetLink, targetPage, size, className }) => {
+    const classNamesElement = classNames(className, 'cta', { cta__small: size === 'small' });
     if (targetLink) {
         return (
-            <a className="cta" href={targetLink}>
+            <a className={classNamesElement} href={targetLink}>
                 {label}
             </a>
         );
@@ -12,7 +14,7 @@ export default ({ label, targetLink, targetPage }) => {
 
     if (targetPage) {
         return (
-            <Link className="cta" to={targetPage?.slug}>
+            <Link className={classNamesElement} to={targetPage?.slug}>
                 {label}
             </Link>
         );
