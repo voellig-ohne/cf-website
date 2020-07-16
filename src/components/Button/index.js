ce296fb3b0ab1ed7a5e88437ce2058de235c898d
Timur Çelikel <me@timurcelikel.com>
2020-07-16 17:34:10 +0200

    Button: fix to internal pages when used inline

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import classNames from 'classnames';
+import { mapFields } from '../contentfulRichText';
 
 export default ({ label, targetLink, targetPage, size, className }) => {
     const classNamesElement = classNames(className, 'cta', { cta__small: size === 'small' });
+
     if (targetLink) {
         return (
             <a className={classNamesElement} href={targetLink}>
@@ -13,8 +15,10 @@ export default ({ label, targetLink, targetPage, size, className }) => {
     }
 
     if (targetPage) {
+        const linktToPage = targetPage.slug ? targetPage.slug : mapFields(targetPage.fields).slug;
+
         return (
-            <Link className={classNamesElement} to={targetPage?.slug}>
+            <Link className={classNamesElement} to={linktToPage}>
                 {label}
             </Link>
         );
