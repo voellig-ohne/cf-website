bc1bb9fec4e06b4fd238c95f38d0acabec2616e7
Timur Çelikel <me@timurcelikel.com>
2020-07-16 17:29:22 +0200

    add section content illustration

---
+++ src/components/SectionContentIllustration/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Forrest from '../Forrest';
+import MiniForrestGroup from '../MiniForrestGroup';
+
+const illustrations = {
+    leistungen: () => <Forrest mode="single" className="section_content-graphic-image" />,
+    workshops: () => <Forrest mode="many" density="0" className="section_content-graphic-image" />,
+    usecases: () => <MiniForrestGroup isSmall={true} className="section_content-graphic-image" />,
+};
+
+export default ({ illustration, children, title }) => {
+    console.log(illustration);
+
+    const IllustrationComponent = illustrations[illustration];
+    return (
+        <section className="section_content">
+            <div className="section_content-main">
+                <h1>{title}</h1>
+                {children}
+            </div>
+            <div className="section_content-secondary">
+                <div className="section_content-graphic">
+                    <IllustrationComponent />
+                </div>
+            </div>
+        </section>
+    );
+};
